aabcfc441a6112f13a09252803ecffcd117711f9
Zakirullah <zakirullah425@gmail.com>
2026-03-10 17:44:41 +0500

    lab files

---
+++ lab 5/lab 3 tasks/lab 5 task 1.cpp
@@ -0,0 +1,21 @@
+                // Name : Zakirullah
+                // Etea ID :5120371/709
+                // section : A 
+                // departement : ELECTRICAL POWER
+
+
+#include <iostream>   // This library is used for input and output
+using namespace std;  // Allows us to use cout and cin without std::
+
+int main()            // Main function where the program starts
+{
+    int i = 1;        // Declare a variable i and initialize it with 1
+
+    while (i <= 10)   // While loop runs as long as i is less than or equal to 10
+    {
+        cout << i << endl;  // Display the value of i on the screen
+        i ++;                // Increase the value of i by 1
+    }
+
+    return 0;        // End of program
+}
